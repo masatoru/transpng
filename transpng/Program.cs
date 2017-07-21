c6a0871555d32399990d4072da14531f18acf72e
Satoru Fujimori <satoru.fujimori@gmail.com>
2017-07-21 15:48:30 +0900

    Add TransparentImage method

--- transpng/Program.cs
+++ transpng/Program.cs
@@ -10,6 +10,26 @@ namespace transpng
     {
         static void Main(string[] args)
         {
+
+
+
         }
+        //透過で上書きする
+        static void TransparentImage(string imgpath)
+        {
+            using (ImageMagick.MagickImage img = new ImageMagick.MagickImage(imgpath))
+            {
+                img.Format = ImageMagick.MagickFormat.Png8;
+                img.Write(imgpath);
+            }
+            using (ImageMagick.MagickImage img = new ImageMagick.MagickImage(imgpath))
+            {
+                img.Format = ImageMagick.MagickFormat.Png8;
+                img.Transparent(new ImageMagick.MagickColor(255, 255, 255));    //White
+                img.ColorFuzz = new ImageMagick.Percentage(10.0);
+                img.Write(imgpath);
+            }
+        }
+
     }
 }
